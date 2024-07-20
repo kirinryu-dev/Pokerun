865528da7a2b8cdb8799ff497bcf9f835f331687
kirinryu.dev <kirinryu.dev@gmail.com>
2024-07-20 09:17:18 +0000

    im about to commit a little bit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { signal , Component, computed } from '@angular/core';
 // import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -8,19 +8,39 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
+
+  imageUrl = signal('https://assets.pokemon.com/assets/cms2/img/pokedex/detail/025.png');
   
-  name = 'Pikachu' ;
-  life = 21 ;
+  name = signal('Pikachu') ;
+  taille = computed(() => {
+    if (this.life()<= 15 ) {
+      // window.prompt('Petit');
+      return 'Petit';
 
+    }
 
+    if (this.life() >= 25 ) {
+      // window.prompt('Grand')
+      return 'Grand'
+    }
 
+    // else {
+    //   window.prompt('Moyen');
+
+    // }
+
+    return 'Moyen';
+
+  })
+  life = signal(22) ;
 
 
   incrementLife () {
     console.log('+1 point de vie !');
 
-    this.life = this.life + 1
+    this.life.update( (n) => n + 1);
 
   }
   
@@ -28,7 +48,7 @@ export class AppComponent {
   decrementLife () {
     console.log('-1 point de vie !');
 
-    this.life = this.life -1 
+    this.life.update( (n) => n - 1 ) ;
 
   }
 
